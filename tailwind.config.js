610005b6969cdae87fd52de17b90494d60b3f3c2
jaspreet tpss <jaspreetstpss@gmail.com>
2025-11-25 22:52:37 +0530

    index file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./landing-page.html",
+    "./**/*.html",
+    "./**/*.{js,ts,jsx,tsx}", // optional, helpful if you add JS files later
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
